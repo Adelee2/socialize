e62205cc10293622f7a948072c0de8701178d7b0
aadewale <aadewale@veritaskapital.com>
2021-06-28 12:27:11 +0100

    chat in progress

--- socket.js
+++ socket.js
@@ -1,10 +1,7 @@
+const app = require('express')();
 const request = require('sync-request');
-const server = require("http").createServer();
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-  },
-});
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
 const SOCKETPORT = process.env.SOCKETPORT || 3032;
 // const baseUrl = process.env.BASE_URL || '/api/v1/';
@@ -13,13 +10,13 @@ const PORT = process.env.PORT || 3000;
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
 
 io.on("connection", (socket) => {
+
   console.log(`Client ${socket.id} connected`);
   const { conversationId } = socket.handshake.query;
   socket.join(conversationId);
   socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
-    // sendMessage(data)
+    sendMessage(data)
     console.log("socket data",data)
-    data.authorization = null
     io.in(conversationId).emit(NEW_CHAT_MESSAGE_EVENT, data);
   });
 
@@ -38,11 +35,9 @@ const sendMessage = (message) => {
     'POST',
     `http://localhost:${PORT}/message/add`,
     { json: {
-        messageListId: message.conversationId,
-        sentBy: message.sentBy,
-        message: message.message,
-      },
-      headers:{'Authorization':message.authorization}});
+        message:message.messages,
+        to:message.toid
+      }});
 }
 const getMessage = (message, conversationId) => {
   var res = request(
